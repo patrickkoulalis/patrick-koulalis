7449b0d22223a8372642c11d425e0bd48f29039c
Patrick Koulalis <patrickkoulalis@Patricks-MacBook-Pro.local>
2018-01-21 16:40:40 -0500

    add routes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // 	}
 // 	res.redirect(`https://${req.hostname}${req.url}`);
 // });
-// app.use('/', routes)
+app.use('/', routes)
 
 
 const https = require('https');
